b7b3d65bcf2a6e20a4cacad0571ece0c82226e15
Douglas Gadêlha <dgadelha@live.com>
2023-11-03 22:39:54 -0300

    fix: sqlite date for metabase

--- src/helpers/sqlite.ts
+++ src/helpers/sqlite.ts
@@ -18,14 +18,14 @@ export async function exportToSqlite(transactions: Transaction[]) {
 
 	await knex.schema.createTable('transactions', table => {
 		table.increments('id');
-		table.string('cartao').comment('Cartão da transação');
-		table.string('fatura').comment('Fatura da transação');
-		table.date('data').comment('Data da transação');
-		table.string('descricao').comment('Descrição da transação');
-		table.string('normalizado').comment('Descrição normalizada da transação');
+		table.text('cartao').comment('Cartão da transação');
+		table.text('fatura').comment('Fatura da transação');
+		table.text('data').comment('Data da transação');
+		table.text('descricao').comment('Descrição da transação');
+		table.text('normalizado').comment('Descrição normalizada da transação');
 		table.decimal('valor').comment('Valor da transação');
-		table.string('parcela').comment('Parcela da transação');
-		table.string('categoria').comment('Categoria da transação');
+		table.text('parcela').comment('Parcela da transação');
+		table.text('categoria').comment('Categoria da transação');
 	});
 
 	const batchSize = 100;
@@ -33,7 +33,12 @@ export async function exportToSqlite(transactions: Transaction[]) {
 	for (let i = 0; i < transactions.length; i += batchSize) {
 		const batch = transactions.slice(i, i + batchSize);
 
-		await knex('transactions').insert(batch);
+		await knex('transactions').insert(
+			batch.map(transaction => ({
+				...transaction,
+				data: transaction.data.toJSON().slice(0, 10),
+			})),
+		);
 	}
 
 	await knex.destroy();
